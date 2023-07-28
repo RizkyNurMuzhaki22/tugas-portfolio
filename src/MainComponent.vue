89d9d9853be46dba6b0480b054f326cfab6746ba
RizkyNurMuzhaki22 <rizki.nurmuzhaki@gmail.com>
2023-07-28 14:38:24 +0700

    Tugas Portfolio

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -67,7 +67,7 @@
     <div>
         <section class="bg-white dark:bg-gray-900">
     <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-        <img class="w-full dark:hidden" src="./components/WhatsApp Image 2023-07-28 at 09.55.50.jpeg" alt="dashboard image">
+        <img class="w-full dark:hidden" src="./components/WhatsApp Image 2023-07-28 at 10.20.57.jpeg" alt="dashboard image">
         <img class="w-full hidden dark:block" src="./components/WhatsApp Image 2023-07-28 at 10.20.57.jpeg" alt="dashboard image">
         <div class="mt-4 md:mt-0">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">About Me</h2>
