0083f12d961324a6236a0b3316d988b30dc8dd7b
RizkyNurMuzhaki22 <rizki.nurmuzhaki@gmail.com>
2023-07-28 15:12:18 +0700

    Tugas Portfolio

--- src/NavbarComponent.vue
+++ src/NavbarComponent.vue
@@ -4,7 +4,7 @@
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="https://flowbite.com" class="flex items-center">
-                <img src="./public/Screenshot 2023-07-28 140419.png" class="mr-3 h-6 sm:h-9" />
+                <img src="/public/Screenshot 2023-07-28 140419.png" class="mr-3 h-6 sm:h-9" />
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Ky.Portfolio</span>
             </a>
             <div class="flex items-center lg:order-2">
