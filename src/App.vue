5dcd989dd805bc0fdf42ccf843a0c46c4a471855
RizkyNurMuzhaki22 <rizki.nurmuzhaki@gmail.com>
2023-07-28 14:27:04 +0700

    Tugas Portfolio

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NavbarComponent/>
+    <HeaderComponent/>
+    <MainComponent/>
+    <FooterComponent/>
+  </div>
+</template>
+
+<script setup>
+import FooterComponent from './FooterComponent.vue';
+import HeaderComponent from './HeaderComponent.vue';
+import MainComponent from './MainComponent.vue';
+import NavbarComponent from './NavbarComponent.vue';
+</script>
---
+++ src/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+<section class="bg-center bg-cover bg-no-repeat bg-[url('https://scontent.fbdo9-1.fna.fbcdn.net/v/t39.30808-6/352830487_3347246875589149_8336255683319163583_n.jpg?stp=dst-jpg_p180x540&_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_ohc=HSSB4DnPFlwAX8PXebE&_nc_ht=scontent.fbdo9-1.fna&oh=00_AfDDLFENEDdJ_z40QGnj88VQeqOgu1kCEZBqNPa-3Q8Q7Q&oe=64C8AC3E')] bg-gray-700 bg-blend-multiply">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+        <div class="flex flex-col justify-center">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome To My Portfolio</h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">" Hanya Lelaki Biasa Yang Pandai Merangkai Kata Namun Belum Juga Bisa Menaklukkan Hatinya "</p>
+            <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                    Lihat Profile
+                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
+                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Baca Selengkapnya -></button>
+        </div>
+    </div>
+    </div>
+</section>
+
+    </div>
+</template>
